a4e2b55aed7d3ac14ce40881072393492966e4d7
yindaheng98 <yindaheng98@163.com>
2021-02-19 15:22:04 +0800

    bug fix

--- ossfs/alpine/mount.sh
+++ ossfs/alpine/mount.sh
@@ -2,6 +2,6 @@
 set -e
 echo $BucketName:$AccessKeyId:$AccessKeySecret > /etc/passwd-ossfs
 chmod 640 /etc/passwd-ossfs
-mkdir -p $MountDir
-ossfs $BucketName $MountDir -ourl=$EndPoint
+mkdir -p $MountPoint
+ossfs $BucketName $MountPoint -ourl=$EndPoint
 exec "$@"
