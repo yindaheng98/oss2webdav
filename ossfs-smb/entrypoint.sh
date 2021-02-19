41c8578fea4a68a342c10728ba47b2977bff398d
yindaheng98 <yindaheng98@163.com>
2021-02-19 21:09:01 +0800

    移到别的地方

--- ossfs-smb/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-echo $BucketName:$AccessKeyId:$AccessKeySecret > /etc/passwd-ossfs
-chmod 640 /etc/passwd-ossfs
-mkdir -p $MountPoint
-ossfs hitomi-downloader $MountPoint -ourl=$EndPoint
-/sbin/tini -- /usr/bin/samba.sh $@
