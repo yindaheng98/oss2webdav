876b7d6353a069ebd4777c357630af66eb069524
yindaheng98 <yindaheng98@163.com>
2021-03-14 14:29:12 +0800

    alpine足矣

--- ossfs/ubuntu/mount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo $BucketName:$AccessKeyId:$AccessKeySecret > /etc/passwd-ossfs
-chmod 640 /etc/passwd-ossfs
-mkdir -p $MountPoint
-ossfs $BucketName $MountPoint -ourl=$EndPoint
-exec "$@"
