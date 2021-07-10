3fee8accdb5f511250f17c9c51bdaf24457a586d
yindaheng98 <yindaheng98@163.com>
2021-07-10 14:56:07 +0800

    添加了cron容器

--- ossfs-nextcloud/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo $BucketName:$AccessKeyId:$AccessKeySecret > /etc/passwd-ossfs
-chmod 640 /etc/passwd-ossfs
-mkdir -p $MountPoint
-ossfs $BucketName $MountPoint -ourl=$EndPoint -o allow_other -ouid=$(id $OWNER_USER -u) -ogid=$(id $OWNER_GROUP -g)
-exec "$@"
