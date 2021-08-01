3f34a0500450d3579d8140ec17dd7968b152e398
yindaheng98 <yindaheng98@163.com>
2021-08-01 22:30:17 +0800

    bug fix

--- v2subscribe/subscribe.balancer.sh
+++ v2subscribe/subscribe.balancer.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 # $1是订阅链接
-# $2是需要多少个outbound
-# $3是生成配置文件的路径
+# $2是生成配置文件的路径
+# $3是需要多少个outbound
+# $4是在前多少个outbound里面选
 PREFIX='v2gen-balance'
 BALANCERS=''
-for i in $(seq 1 $2); do
+SELECT=0
+for i in $(seq 1 $3); do
     UUID=$(cat /proc/sys/kernel/random/uuid)
     TEMP=/tmp/${UUID}.json
-    v2gen -u "$1" -o "$TEMP" -template /etc/v2ray/templates/outbound.template.json -best
+    expect << EOF
+    set timeout -1
+    spawn v2gen -u "$1" -o $TEMP -template /etc/v2ray/templates/outbound.template.json -thread $THREAD -c $COUNT
+    expect "Please Select:"
+    send "$(expr $RANDOM % $4)\n"
+    expect "config has been written to"
+EOF
     TAG_PREFIX="$PREFIX-$i-"
     content=$(cat "$TEMP")
     eval "cat <<EOF
@@ -20,5 +28,5 @@ SELECTORS="\"$PREFIX\","
 content=$(cat /etc/v2ray/templates/balancer.template.json)
 eval "cat <<EOF
 $content
-EOF" >"$3"
-cat "$3"
+EOF" >"$2"
+cat "$2"
